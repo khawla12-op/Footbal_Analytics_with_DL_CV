8c8060b1fd6cef0e354b0f64672f7bef87120b92
khaoula <khawlaallak@gmail.com>
2024-09-21 20:47:55 +0100

    New project

--- Streamlit_web_app/main.py
+++ Streamlit_web_app/main.py
@@ -12,9 +12,7 @@ from detection import create_colors_info, detect
 def main():
 
     st.set_page_config(page_title="AI Powered Web Application for Football Tactical Analysis", layout="wide", initial_sidebar_state="expanded")
-    st.title("Football Players Detection With Team Prediction & Tactical Map")
-    st.subheader(":red[Works only with Tactical Camera footage]")
-
+    st.title("Football Players Detection")
     st.sidebar.title("Main Settings")
     demo_selected = st.sidebar.radio(label="Select Demo Video", options=["Demo 1", "Demo 2"], horizontal=True)
 
@@ -76,31 +74,9 @@ def main():
     st.sidebar.markdown('---')
 
     ## Page Setup
-    tab1, tab2, tab3 = st.tabs(["How to use?", "Team Colors", "Model Hyperparameters & Detection"])
-    with tab1:
-        st.header(':blue[Welcome!]')
-        st.subheader('Main Application Functionalities:', divider='blue')
-        st.markdown("""
-                    1. Football players, referee, and ball detection.
-                    2. Players team prediction.
-                    3. Estimation of players and ball positions on a tactical map.
-                    4. Ball Tracking.
-                    """)
-        st.subheader('How to use?', divider='blue')
-        st.markdown("""
-                    **There are two demo videos that are automaticaly loaded when you start the app, alongside the recommended settings and hyperparameters**
-                    1. Upload a video to analyse, using the sidebar menu "Browse files" button.
-                    2. Enter the team names that corresponds to the uploaded video in the text fields in the sidebar menu.
-                    3. Access the "Team colors" tab in the main page.
-                    4. Select a frame where players and goal keepers from both teams can be detected.
-                    5. Follow the instruction on the page to pick each team colors.
-                    6. Go to the "Model Hyperpramerters & Detection" tab, adjust hyperparameters and select the annotation options. (Default hyperparameters are recommended)
-                    7. Run Detection!
-                    8. If "save outputs" option was selected the saved video can be found in the "outputs" directory
-                    """)
-        st.write("Version 0.0.1")
+    tab1, tab2= st.tabs(["Team Colors", "Model Hyperparameters & Detection"])
 
-    with tab2:
+    with tab1:
         t1col1, t1col2 = st.columns([1,1])
         with t1col1:
             cap_temp = cv2.VideoCapture(tempf.name)
@@ -172,7 +148,7 @@ def main():
                                                      team2_name, st.session_state[f"{team2_name} P color"], st.session_state[f"{team2_name} GK color"])
 
 
-    with tab3:
+    with tab2:
         t2col1, t2col2 = st.columns([1,1])
         with t2col1:
             player_model_conf_thresh = st.slider('PLayers Detection Confidence Threshold', min_value=0.0, max_value=1.0, value=0.6)
